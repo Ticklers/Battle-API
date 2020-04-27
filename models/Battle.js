ff34fa7db3d9670ae403090ecb2bb76ca4ed22da
Ishan Gupta <ishangupta004@gmail.com>
2020-04-27 23:16:56 +0530

    Battle model added

---
+++ models/Battle.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BattleSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    random: {
+        type: Boolean,
+        default: true
+    },
+    invitationLink: {
+        type: String
+    },
+    startDate: {
+        type: Date,
+        default: Date.now()
+    },
+    endDate: {
+        type: Date,
+        default: Date.now()
+    },
+    teamOne: [
+        {
+            user: {
+                userId : {
+                    type: Schema.Types.ObjectId,
+                    ref: 'users'
+                },
+                memes: [
+                    {
+                        meme: {
+                        type: Schema.Types.ObjectId,
+                        ref: 'memes'
+                        }
+                    }
+                ]
+            }
+        }
+    ],
+    teamTwo: [
+        {
+            user: {
+                userId : {
+                    type: Schema.Types.ObjectId,
+                    ref: 'users'
+                },
+                memes: [
+                    {
+                        meme: {
+                        type: Schema.Types.ObjectId,
+                        ref: 'memes'
+                        }
+                    }
+                ]
+            }
+        }
+    ],
+    teamOneWins: {
+        type: Boolean,
+        default: true
+    },
+})

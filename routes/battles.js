dbf4ce9e6ae2323c04a07eb4dda697865f44cf76
Ishan Gupta <ishangupta004@gmail.com>
2020-04-29 22:11:43 +0530

    validation to all the routes added

--- routes/battles.js
+++ routes/battles.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const User = require('../models/User');
 const Meme = require('../models/Meme');
 const Battle = require('../models/Battle');
+var _ = require('lodash');
 const host = 'http://localhost:5000/'
 
 router.get('/test', (req, res) => {
@@ -11,6 +12,9 @@ router.get('/test', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
+    if(_.isEmpty(req.body.name)) {
+        return res.json({EmptyField: "please enter the name of battle"});
+    }
     const newBattle = new Battle({
         name: req.body.name
     })
@@ -19,17 +23,27 @@ router.post('/create', (req, res) => {
 
 router.post('/getinvitation/:id', (req, res) => {
     Battle.findById(req.params.id).then(battle => {
+        if(battle) {
         battle.invitationLink = host + 'api/battles/' + battle.id;
         battle.save().then(battle => {res.json({invitationLink: battle.invitationLink})})
                     .catch(err => {console.log(err)});
+        }
+        else {
+            return res.status(400).json({notFound: "battle not found with given id"});
+        }
     })
 })
 
 router.get('/find/:id', (req, res) => {
     Battle.findById(req.params.id)
-            .then(battle => {
-                res.status(200).json(battle);
-            })
+        .then(battle => {
+            if(battle) {
+            res.status(200).json(battle);
+            }
+            else{
+                return res.status(400).json({notFound: "battle not found with given id"});
+            }
+        })
 })
 
 router.get('/all', (req, res) => {

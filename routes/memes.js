2f17f45fd3dc35c24e24b5d5e6cc1c075e5a65be
Rishabh <princerishabh.sharma400@gmail.com>
2020-04-30 12:41:54 +0530

    remove authorization from media

--- routes/memes.js
+++ routes/memes.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 const Meme = require('../models/Meme');
 
 router.get('/test', (req, res) => {
-    res.json({ msg: 'Meme route work' })
+  res.json({ msg: 'Meme route work' })
 });
 
 const multer = require("multer");
@@ -25,89 +25,90 @@ const fileFilter = (req, file, cb) => {
     cb(null, false);
   }
 };
-const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5}, fileFilter: fileFilter });
+const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 5 }, fileFilter: fileFilter });
 
 const validateMemesInput = require('../validation/meme');
 router.post('/post', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const { errors, isValid } = validateMemesInput(req.body);
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    const newMeme = new Meme({
-        caption: req.body.caption,
-        media: req.body.media,
-        user: req.user.id
-    });
-
-    newMeme.save().then(meme => res.json(meme));
-    User.findOne({ username: req.user.username }).then(user => {
-        if (!user) {
-            console.log('no user found');
-            console.log(newMeme.user);
-        }
-        user.memes.unshift({ meme: newMeme.id });
-        user.save();
-    }).catch(err => console.log(err));
-});
-
-router.patch("/media/:id",passport.authenticate('jwt', { session: false }), upload.single('media'), (req, res, next) => {
-    const id = req.params.id;
-    const updateOps = {
-      media: req.file.path
-    };
-    console.log(req.file.path);
-    Meme.update({ _id: id }, { $set: updateOps })
-      .exec()
-      .then(result => {
-        res.status(200).json({
-          message: "Media uploaded",
-          request: {
-            type: "GET",
-            url: "http://localhost:5000/api/memes/findmeme/" + id
-          }
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
+  const { errors, isValid } = validateMemesInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const newMeme = new Meme({
+    caption: req.body.caption,
+    media: req.body.media,
+    user: req.user.id
   });
 
+  newMeme.save().then(meme => res.json(meme));
+  User.findOne({ username: req.user.username }).then(user => {
+    if (!user) {
+      console.log('no user found');
+      console.log(newMeme.user);
+    }
+    user.memes.unshift({ meme: newMeme.id });
+    user.save();
+  }).catch(err => console.log(err));
+});
+
+router.patch("/media/:id", upload.single('media'), (req, res, next) => {
+  const id = req.params.id;
+  const updateOps = {
+    media: req.file.path
+  };
+  console.log(req.file.path);
+  Meme.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: "Media uploaded",
+        request: {
+          type: "GET",
+          url: "http://localhost:5000/api/memes/findmeme/" + id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 router.get('/all', (req, res) => {
-    Meme.find().sort({ date: -1 })
-        .then(memes => {
-            const response = {
-                count: memes.length,
-                memes
-            };
-            res.json({ response })
-        })
-        .catch(error => res.status(404).json({ noMemesFound: 'no Meme found' }));
+  Meme.find().sort({ date: -1 })
+    .then(memes => {
+      const response = {
+        count: memes.length,
+        memes
+      };
+      res.json({ response })
+    })
+    .catch(error => res.status(404).json({ noMemesFound: 'no Meme found' }));
 });
 
 router.get('/findmeme/:id', (req, res) => {
-    Meme.findById(req.params.id)
-        .then(meme => {
-            res.json(meme)})
-        .catch(error => res.status(404).json({ noMemeFound: 'no Meme found with given ID' }));
+  Meme.findById(req.params.id)
+    .then(meme => {
+      res.json(meme)
+    })
+    .catch(error => res.status(404).json({ noMemeFound: 'no Meme found with given ID' }));
 });
 
 router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-    User.findById(req.user.id).then(user => {
-        Meme.findById(req.params.id)
-            .then(meme => {
-                const removeIndex = user.memes
-                    .map(item => item.meme.toString())
-                    .indexOf(req.params.id);
-                meme.remove().then(() => res.json({ success: true }))
-                                .catch(error => res.json(error));
-                user.memes.splice(removeIndex, 1);
-                user.save();
-            })
-            .catch(err => console.log(err));
-    });
+  User.findById(req.user.id).then(user => {
+    Meme.findById(req.params.id)
+      .then(meme => {
+        const removeIndex = user.memes
+          .map(item => item.meme.toString())
+          .indexOf(req.params.id);
+        meme.remove().then(() => res.json({ success: true }))
+          .catch(error => res.json(error));
+        user.memes.splice(removeIndex, 1);
+        user.save();
+      })
+      .catch(err => console.log(err));
+  });
 });
 
 module.exports = router;

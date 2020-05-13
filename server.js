b42a93a17c2f7ff41211cb13a259c181e85499f1
ishan <ishangupta004@gmail.com>
2020-05-14 00:03:54 +0530

    typing errors removed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(function (req, res, next) {
 
   if (req.method === "OPTIONS") {
     res.header(
-      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Methods",
       "PUT, POST, PATCH, DELETE, GET, OPTIONS"
     );
     return res.status(200).json({});
